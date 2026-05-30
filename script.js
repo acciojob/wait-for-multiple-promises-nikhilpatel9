e78b3f90c4c93f73626c4a50cde82028ecaae6e7
NIKHIL PATEL <115173157+nikhilpatel9@users.noreply.github.com>
2026-05-30 11:01:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -26,8 +26,8 @@ Promise.all([Promise1, Promise2, Promise3]).then((values) => {
     
      const totalTime = Math.max(p1Time, p2Time, p3Time);
 
-    tabBody.innerHTML += `<tr><td>Promise 1</td><td>${p1Time.toFixed(3)} s</td></tr>`;
-    tabBody.innerHTML += `<tr><td>Promise 2</td><td>${p2Time.toFixed(3)} s</td></tr>`;
-    tabBody.innerHTML += `<tr><td>Promise 3</td><td>${p3Time.toFixed(3)} s</td></tr>`;
-    tabBody.innerHTML += `<tr><td><strong>Total</strong></td><td><strong>${totalTime.toFixed(3)} s</strong></td></tr>`;
+    tabBody.innerHTML += `<tr><td>Promise 1</td><td>${p1Time.toFixed(3)}</td></tr>`;
+    tabBody.innerHTML += `<tr><td>Promise 2</td><td>${p2Time.toFixed(3)}</td></tr>`;
+    tabBody.innerHTML += `<tr><td>Promise 3</td><td>${p3Time.toFixed(3)}</td></tr>`;
+    tabBody.innerHTML += `<tr><td><strong>Total</strong></td><td><strong>${totalTime.toFixed(3)}</strong></td></tr>`;
 });
